1530e1d7064d71a73a38a1d5c88522c7c64a00a0
JonasAntonio <jayv.jonas@gmail.com>
2018-08-25 17:39:26 -0300

    CRUD de Categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -206,9 +207,7 @@ $app->post("/admin/forgot/reset", function(){
 
 	$user->get((int)$forgot["iduser"]);
 
-	//encodePassword();
-	//$password = password_hash($_POST["password"], PASSWORD_DEFAULT, ['cost'=>12]);
-	$password = User::encodePassword($_POST["password"]);//testar reset password
+	$password = User::encodePassword($_POST["password"]);
 
 	$user->setPassword($password);
 
@@ -219,6 +218,91 @@ $app->post("/admin/forgot/reset", function(){
 
 	$page->setTpl("forgot-reset-success");
 
+});
+
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit();
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+	exit;
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){//rota para a página de edição de categoria
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", array(
+		"category"=>$category->getValues()
+	));
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){//rota que edita a categoria
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+	exit;
 });
 
 $app->run();
